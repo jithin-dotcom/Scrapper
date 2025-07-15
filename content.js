62dabfba60adfa65b553b62246c05809af2d1419
jithin-dotcom <jithinp2906@gmail.com>
2025-07-15 15:32:42 +0530

    residual files removed

--- content.js
+++ content.js
@@ -1,38 +1,3 @@
-// chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-//   if (msg.action === "getImages") {
-//     (async function () {
-//       // Scroll to load lazy images
-//       const autoScroll = () => {
-//         return new Promise((resolve) => {
-//           let totalHeight = 0;
-//           const distance = 300;
-//           const timer = setInterval(() => {
-//             window.scrollBy(0, distance);
-//             totalHeight += distance;
-//             if (totalHeight >= document.body.scrollHeight - window.innerHeight) {
-//               clearInterval(timer);
-//               resolve();
-//             }
-//           }, 300);
-//         });
-//       };
-
-//       await autoScroll();
-//       await new Promise(res => setTimeout(res, 2000)); // wait for lazy images
-
-//       const images = Array.from(document.querySelectorAll("img")).map(img =>
-//         img.src || img.getAttribute("data-src") || img.getAttribute("data-lazy")
-//       ).filter(Boolean);
-
-//       sendResponse({ images });
-//     })();
-
-//     return true; // keep message channel open
-//   }
-// });
-
-
-
 
 
 chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
@@ -43,12 +8,12 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
         return new Promise((resolve) => {
           let totalHeight = 0;
           const distance = 300;
-          const maxScroll = 5000; // Scroll max this much (you can increase)
+          const maxScroll = 5000;
           const timer = setInterval(() => {
             window.scrollBy(0, distance);
             totalHeight += distance;
 
-            // End scroll if we reach bottom or maxScroll limit
+           
             if (
               totalHeight >= document.body.scrollHeight - window.innerHeight ||
               totalHeight >= maxScroll
@@ -61,7 +26,7 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
       };
 
       await autoScroll();
-      await new Promise((res) => setTimeout(res, 2000)); // Allow time for images to load
+      await new Promise((res) => setTimeout(res, 2000)); 
 
       const images = Array.from(document.querySelectorAll("img")).map(img =>
         img.src || img.getAttribute("data-src") || img.getAttribute("data-lazy")
@@ -70,6 +35,6 @@ chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
       sendResponse({ images });
     })();
 
-    return true; // Keep message channel open
+    return true; 
   }
 });
